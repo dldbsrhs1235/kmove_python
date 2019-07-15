341351bc35edc0c420b839ecbf42216209ddea4e
dldbsrhs1235 <dldbsrhs1235@naver.com>
2019-07-15 17:46:00 +0900

    20190715 수업

--- basic/test.py
+++ basic/test.py
@@ -3,4 +3,22 @@ print('hi')
 print('''hi
          hi
          hi''')
-         
+
+food="python'\n favorite "
+print(food)
+print('123','한글','영어',sep='-',end='')
+print('다음줄임')
+head="python"
+tail="is fun!"
+print(head + tail)
+number1="1"
+number2="2"
+print(number1+number2)
+print(1+2+3+4)
+print(head*3)
+print("="*50)
+print(len(food))
+print(food[-5])
+print('i eat %d apples.'%4)
+a=7
+print('i hate %d apples.%d' %(a,7))
